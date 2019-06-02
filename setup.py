694138369947be626cba29bb8c7193b734e545f4
Adam Heins <mail@adamheins.com>
2019-06-02 16:18:52 -0400

    Initial package setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xcp-tool',
+      version='0.1',
+      description='Command line tool for cutting, copying, and pasting files.',
+      url='https://github.com/adamheins/xcp',
+      author='Adam Heins',
+      author_email='mail@adamheins.com',
+      license='MIT',
+      packages=['xcp'],
+      zip_safe=False)
