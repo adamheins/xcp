f9cd8aed6032d954c82f2bb5c060468f4899a04e
Adam Heins <mail@adamheins.com>
2019-07-10 19:22:54 -0400

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+install_requires = ['colorama', 'yaml']
+test_requires = ['pytest']
+
 setup(name='xcp-tool',
       version='0.1',
       description='Command line tool for cutting, copying, and pasting files.',
       url='https://github.com/adamheins/xcp',
       author='Adam Heins',
       author_email='mail@adamheins.com',
+      install_requires=install_requires,
+      test_requires=test_requires,
       license='MIT',
       packages=['xcp'],
       scripts=['bin/xcp'],
