f1f4238c65733870e6de3a0123b66787940b8b3a
Adam Heins <mail@adamheins.com>
2021-01-16 18:27:30 -0500

    Version 0.1.1.

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup
 install_requires = ['colorama', 'pyyaml']
 test_requires = ['pytest']
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(name='xcp-tool',
-      version='0.1',
+      version='0.1.1',
       description='Command line tool for cutting, copying, and pasting files.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/adamheins/xcp',
       author='Adam Heins',
       author_email='mail@adamheins.com',
@@ -14,5 +19,5 @@ setup(name='xcp-tool',
       license='MIT',
       packages=['xcp'],
       scripts=['bin/xcp'],
-      python_requires='>=3',
+      python_requires='>=3.5',
       zip_safe=False)
