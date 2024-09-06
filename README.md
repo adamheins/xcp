925a3fb5b2fe18520f89f762140393fe4645ba78
Adam Heins <mail@adamheins.com>
2024-09-06 17:54:19 -0400

    Empty clipboard after xmv, check that files exist, add usage info to README.

--- README.md
+++ README.md
@@ -6,5 +6,25 @@ post](https://adamheins.com/blog/cut-copy-paste-files-cli).
 I had previously implemented similar functionality as a much more complicated
 Python tool, which you can see in earlier commits of this repo.
 
+## Setup
+
+Arrange for `xcp.sh` to be sourced by your shell, and run
+```bash
+mkdir ~/.xcp
+```
+once before using it.
+
+## Usage
+
+```bash
+xmark <files>  # add some files to the clipboard
+xcp <dest>     # copy clipboard files somewhere
+xmv <dest>     # move clipboard files somewhere
+```
+
+Note that the clipboard files can be copied to as many destinations as desired,
+but they can only be moved once.
+
 ## License
+
 MIT
--- xcp.sh
+++ xcp.sh
@@ -12,8 +12,17 @@ xmark() {
 xcp() {
   local files
   while read line; do
-    files+=("$line")
+    # only add real files
+    if [ -e "$line" ]; then
+      files+=("$line")
+    fi
   done < "$CLIPBOARD_FILE"
+
+  if [ -z "$files" ]; then
+    echo "no files in clipboard"
+    return 1
+  fi
+
   # flags:
   # -i  prompt before overwrite
   # -v  say what is being done
@@ -25,7 +34,18 @@ xcp() {
 xmv() {
   local files
   while read line; do
-    files+=("$line")
+    if [ -e "$line" ]; then
+      files+=("$line")
+    fi
   done < "$CLIPBOARD_FILE"
+
+  if [ -z "$files" ]; then
+    echo "no files in clipboard"
+    return 1
+  fi
+
   mv -iv $files $@
+
+  # clear the clipboard now that files are moved
+  echo > $CLIPBOARD_FILE
 }
